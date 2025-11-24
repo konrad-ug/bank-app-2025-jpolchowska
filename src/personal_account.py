5d77aeeb312b3fe48c2193c3533fbb4fd1688ae0
Jagoda Połchowska <j.polchowska.252@studms.ug.edu.pl>
2025-11-24 22:56:51 +0100

    Refactor + Feature 5 + tests

--- src/personal_account.py
+++ src/personal_account.py
@@ -5,11 +5,15 @@ class PersonalAccount(Account):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 50.0 if self.is_promo_code_valid(promo_code) else 0.0
         self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        self.promo_code = promo_code
+        if self.is_promo_code_valid(promo_code) and self.yob_from_pesel() > 1960:
+            self.balance = 50.0
+        else:
+            self.balance = 0.0
         
     def is_pesel_valid(self, pesel):
-        if len(pesel) == 11 and pesel.isdigit():
+        if isinstance(pesel, str) and len(pesel) == 11 and pesel.isdigit():
             return True
         return False
         
@@ -20,6 +24,18 @@ class PersonalAccount(Account):
             return True
         return False
     
+    def yob_from_pesel(self):
+        pesel = self.pesel
+        if pesel.isdigit():
+            year = int(pesel[:2])
+            month = int(pesel[2:4])
+            if 1 <= month <= 12:
+                return 1900 + year
+            elif 21 <= month <= 32:
+                return 2000 + year
+        else:
+            return 0
+
     def express_transfer(self, amount: float) -> None:
         fee = 1.0
         super().express_transfer(amount, fee)
--- tests/unit/test_company_account.py
+++ tests/unit/test_company_account.py
@@ -1,23 +1,32 @@
 from src.company_account import CompanyAccount
 
 class TestCompanyAccount:
-    def test_account_creation(self):
+    def test_company_account_creation(self):
         account = CompanyAccount("Tech Solutions", "1234567890")
         assert account.company_name == "Tech Solutions"
         assert account.nip == "1234567890"
+        assert account.balance == 0.0
 
-    def test_account_creation_with_invalid_nip_too_short(self):
-        account = CompanyAccount("Business Corp", "12345")
+    def test_nip_empty(self):
+        account = CompanyAccount("Tech Solutions", "")
         assert account.nip == "Invalid"
 
-    def test_account_creation_with_invalid_nip_too_long(self):
+    def test_nip_none(self):
+        account = CompanyAccount("Tech Solutions", None)
+        assert account.nip == "Invalid"
+
+    def test_nip_too_short(self):
+        account = CompanyAccount("Tech Solutions", "123")
+        assert account.nip == "Invalid"
+
+    def test_nip_too_long(self):
         account = CompanyAccount("Tech Solutions", "1234567891011")
         assert account.nip == "Invalid"
     
-    def test_account_creation_with_invalid_nip_with_letters(self):
-        account = CompanyAccount("Tech Solutions", "123A56B89C")
+    def test_nip_with_letters(self):
+        account = CompanyAccount("Tech Solutions", "123456789A")
         assert account.nip == "Invalid"
 
-    def test_account_creation_with_invalid_nip_empty(self):
-        account = CompanyAccount("Tech Solutions", "")
-        assert account.nip == "Invalid"
+    def test_nip_correct(self):
+        account = CompanyAccount("Tech Solutions", "9492458300")
+        assert account.nip == "9492458300"
