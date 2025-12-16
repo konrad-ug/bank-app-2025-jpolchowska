91013cd01671c9cdf81c426ba49766278e434f48
Jagoda Połchowska <j.polchowska.252@studms.ug.edu.pl>
2025-12-16 14:09:01 +0100

    Feature 17

--- src/account.py
+++ src/account.py
@@ -3,19 +3,25 @@ class Account:
         self.balance = 0.0
         self.history = []
 
-    def incoming_transfer(self, amount: float) -> None:
+    def incoming_transfer(self, amount: float) -> bool:
         if amount > 0:
             self.balance += amount
             self.history.append(amount)
+            return True
+        return False
 
-    def outgoing_transfer(self, amount: float) -> None:
+    def outgoing_transfer(self, amount: float) -> bool:
         if amount > 0 and amount <= self.balance:
             self.balance -= amount
             self.history.append(-amount)
+            return True
+        return False
 
-    def express_transfer(self, amount: float, fee: float) -> None:
+    def express_transfer(self, amount: float, fee: float) -> bool:
         total = amount + fee
         if amount > 0 and self.balance - total >= -fee:
             self.balance -= total
             self.history.append(-amount)
             self.history.append(-fee)
+            return True
+        return False
